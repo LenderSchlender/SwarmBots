8ec3d848f40462fd57819a2fbb6b89b19b818096
LenderSchlender <leander@gastgeber.at>
2025-02-19 12:00:34 +0100

    Remove legacy feature

--- robots/commons/src/Robota.h
+++ robots/commons/src/Robota.h
@@ -10,8 +10,6 @@
 class Robota : Module {
   // Global tick counter for modules that do tasks every n ticks
   uint32_t ticks;
-  // Robot dimensions (in µm, along the x/y/z axis)
-  uint16_t width, length, height;
   // Stored modules & types for quick type checking
   uint16_t moduleTypes[MAX_MODULE_AMOUNT];
   Module* modules[MAX_MODULE_AMOUNT];
@@ -21,8 +19,6 @@ public:
   void init();
   void tick();
   uint32_t getTicks();
-  void setDimensions(uint16_t width, uint16_t length, uint16_t height);
-
   // returns the module index if successful, or -1 if failed
   int16_t addModule(Module* module);
 
