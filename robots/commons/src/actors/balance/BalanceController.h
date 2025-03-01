d118472a69a67330b00b37ea403a7249af888248
LenderSchlender <leander@gastgeber.at>
2025-03-01 22:08:25 +0100

    Chore: Update Styling

--- robots/commons/src/actors/balance/BalanceController.h
+++ robots/commons/src/actors/balance/BalanceController.h
@@ -8,22 +8,22 @@
 
 // For balancing on one axis
 class BalanceController : public Module {
-    SingleMotorOutput *left, *right;
-    RotaryEncoder *leftEnc, *rightEnc;
-    MPU6050 *mpu;
+  SingleMotorOutput *left, *right;
+  RotaryEncoder *leftEnc, *rightEnc;
+  MPU6050 *mpu;
 
-    KalmanFilter kalman;
-    // Calculated angle of the robot using the Kalman filter
-    // positive value = falling forwards
-    float angle;
-    // Read data from IMU and calculate current robot angle
-    void updateAngle();
+  KalmanFilter kalman;
+  // Calculated angle of the robot using the Kalman filter
+  // positive value = falling forwards
+  float angle;
+  // Read data from IMU and calculate current robot angle
+  void updateAngle();
 
 public:
-    BalanceController(SingleMotorOutput *left, SingleMotorOutput *right,
-                      RotaryEncoder *leftEnc, RotaryEncoder *rightEnc, MPU6050 *mpu);
-    void init();
-    void tick();
+  BalanceController(SingleMotorOutput *left, SingleMotorOutput *right,
+                    RotaryEncoder *leftEnc, RotaryEncoder *rightEnc, MPU6050 *mpu);
+  void init();
+  void tick();
 };
 
 #endif
