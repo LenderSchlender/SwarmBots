cc45abd0aa66e078b2d1b9a04dda762dea60e4ef
LenderSchlender <leander@gastgeber.at>
2025-03-09 23:53:28 +0100

    Automatic protobuf compilation using vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { exec } from 'node:child_process'
+import fs from "fs"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +14,32 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    {
+      name: "vite-compile-protobufs",
+      buildStart(options) {
+        // Make sure output directory exists
+        if (!fs.existsSync("./src/lib")) {
+          console.log("Creating dir for compiled protobufs...")
+          fs.mkdirSync("./src/lib")
+        }
+
+        // Make sure old versions have been disposed of
+        fs.readdirSync("./src/lib").forEach(file => {
+          fs.rmSync(path.join("./src/lib", file));
+        });
+
+        // Compile protobufs
+        exec("npx protoc --proto_path=../proto --ts_out=./src/lib/ ../proto/*.proto", (error, stdout, stderr) => {
+            if (error)
+              console.error('\n'+error.message)
+            if (stderr)
+              console.error('\n'+stderr)
+            if (stdout)
+              console.log('\n'+stdout)
+        })
+        console.log("Compiled protobufs!")
+      },
+    }
   ],
   resolve: {
     alias: {
